507df217d221a76dc11fbfd2a760fc44f1c9c55a
Markus Friis Hansen <mark6211@edu.aarhustech.dk>
2026-04-02 08:44:24 +0200

    added relations

--- Utils/preprocessing_upd.py
+++ Utils/preprocessing_upd.py
@@ -40,7 +40,7 @@ def extracts_articles(df: pd.DataFrame) -> pd.DataFrame:
     return articles_df
 
 
-def data_preprocessing(df: DataFrame) -> DataFrame:
+def data_preprocessing(df: DataFrame) -> list:
     
     articles = extracts_articles(df)
     entities = extracts_entities(df)
@@ -154,8 +154,9 @@ def change_index(text: str, text_span: str, start_idx: int, end_idx: int) -> Tup
             msg = f"text_span {text_span} does not capture the entire word: {text[start_idx:end_idx]}"
             warnings.warn(msg)
     
-    if end_idx < len(text):
-        if re.match(r"[^\d\w]", text[end_idx]) is None:
+    if end_idx < (len(text) - 1):
+        char = text[end_idx + 1]
+        if char.isdigit() or char.isalpha():
             end_idx += 1
             msg = f"text_span {text_span} does not capture the entire word: {text[start_idx:end_idx]}"
             warnings.warn(msg)
@@ -247,6 +248,74 @@ def main(datatype: str):
     os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path, "wb") as f:
         pickle.dump(corrected_df, f)
+
+def preprocessing():
+    
+    results = []
+
+for idx, annotation in df_test.iterrows():
+    pmid = annotation.pmid
+    location = annotation.location
+    text = annotation.text
+
+    entry = (
+        text,
+        {"pmid": pmid,
+         "location": location,
+         "annotatpor": annotation.annotator,
+         "entities": [],
+         "text_span": [],
+         "relations": []}
+    )
+
+    entities = entities_df[(entities_df["pmid"] == pmid) & (entities_df["location"] == location)]
+    relations = relations_df[(relations_df["pmid"] == pmid) & (relations_df["subject_location"] == location)]
+    cleaned_text = clean_html_tags(text)
+    for _, entity in entities.iterrows():
+        text_span = entity.text_span
+        start = entity.start_idx
+        end = entity.end_idx
+        if not relations.empty:
+            subject_entity = relations[relations["subject_start_idx"] == start]
+            object_entity = relations[relations["object_start_idx"] == start]
+
+        while True:
+            new_start_idx, new_end_idx = change_index(text, text_span, start, end)
+            if new_start_idx == start and new_end_idx == end:
+                break
+            start = new_start_idx
+            end = new_end_idx
+            text_span = text[start:end]
+
+            if text_span.startswith("Background"):
+                text_span = text[start+10:end]
+                break
+
+        if not valid_text_span(text_span):
+            print(f"Not valid {text_span}")
+            if not relations.empty:
+                relations.where(relations["subject_start_idx"] == start, -1)
+                relations.where(relations["object_start_idx"] == start, -1)
+            continue
+        
+        if not relations.empty:
+            relations.where(relations["subject_start_idx"] == start, start)
+            relations.where(relations["object_start_idx"] == start, start)
+
+        entry[1]["entities"].append((start, end, entity.label))
+        entry[1]["text_span"].append(text_span)
+
+
+    for _, relation in relations.iterrows():
+        subject_start = relation.subject_start_idx
+        object_start = relation.object_start_idx
+        predicate = relation.predicate
+
+        if subject_start != -1 or object_start != -1:
+            continue
+
+        entry[1]["relations"].append((subject_start, predicate, object_start))    
+    results.append(entry)
 
 
 if __name__ == "__main__":
